08c5dff80651722db57b70ef7b80d009a4ead8f0
michmato <michal.matoga@schibsted.pl>
2016-04-02 13:54:45 +0200

    Add tests template

---
+++ test/Acme/ApplicationTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Acme;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRun()
+    {
+        $this->assertEquals(0, 0);
+    }
+    
+    public function testRun2()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
